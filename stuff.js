1e8cbc0e1560bb09684198d389b9deac6c9714f7
hwalborn <hwalborn@gmail.com>
2017-06-13 12:40:27 -0400

    webpack and npm, what what

--- stuff.js
+++ stuff.js
@@ -1,1 +1,9 @@
-console.log('stuff is currently loading...')
+
+let checkName= (firstName, lastName) => {
+ if(firstName !== 'nader' || lastName !== 'dabit') {
+   console.log('You are not Nader Dabit');
+ } else {
+    console.log('You are Nader Dabit');
+  }
+}
+checkName('nader', 'jackson');
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,27 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'jshint-loader'
+      }
+   ],
     loaders: [
-      { test: /\.css$/, loader: "style!css" }
+      { test: /\.css$/, loader: "style!css" },
+      {
+       test: /\.js$/,
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['react', 'es2015']
+       }
+     }
     ]
+  },
+  resolve: {
+    extensions: ['.js']
   }
 }
